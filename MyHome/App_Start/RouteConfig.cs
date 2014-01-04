abdb77aa696a024d4e1b3cf695c00867977601e4
Juvaly <juvaly@gmail.com>
2014-01-04 23:48:19 +0200

    localization seo optimization

--- MyHome/App_Start/RouteConfig.cs
+++ MyHome/App_Start/RouteConfig.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Classy.DotNet.Mvc.Localization;
 
 namespace MyHome
 {
@@ -62,11 +63,8 @@ namespace MyHome
 
             #endregion 
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            var defaultController = new Classy.DotNet.Mvc.Controllers.DefaultController();
+            defaultController.RegisterRoutes(routes);
         }
     }
 }
