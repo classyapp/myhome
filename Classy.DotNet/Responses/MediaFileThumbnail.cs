5a701aa9d7149719edc1f054ab8d6567ac6e85b9
Noah Blumenthal <noah@hackerhasid.com>
2014-01-14 00:24:37 -0500

    added MediaFileThumbnail class

--- Classy.DotNet/Responses/MediaFileThumbnail.cs
+++ Classy.DotNet/Responses/MediaFileThumbnail.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace Classy.DotNet.Responses
 {
-    public enum MediaFileType
+    public class MediaFileThumbnail
     {
-        Image = 1 << 0
-    }
-
-    public class MediaFileView
-    {
+        public int Height { get; set; }
+        public int Width { get; set; }
         public string Key { get; set; }
-        public string ContentType { get; set; }
         public string Url { get; set; }
-        public IList<MediaFileThumbnail> Thumbnails { get; set; }
     }
 }
