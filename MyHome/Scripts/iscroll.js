30da539c23cee197f9693e0d3fe3d6c04ff7054b
Michael Bar <barmic@gmail.com>
2014-03-03 13:50:09 -0300

    QueryString Script fix

--- MyHome/Scripts/iscroll.js
+++ MyHome/Scripts/iscroll.js
@@ -39,11 +39,14 @@
 function parseQueryString() {
     var data = {};
     var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        data[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    if (query != "") {
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            data[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+        }
     }
+    return data;
 }
 
 function resetPagination(page) {
