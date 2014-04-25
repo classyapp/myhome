98417cfa72d1f9f5364fa4c31d965d6a6369f43d
Michael Bar <barmic@gmail.com>
2014-04-25 16:48:43 -0300

    Minor fixes

--- Classy.DotNet.Mvc/Controllers/ProfileController.cs
+++ Classy.DotNet.Mvc/Controllers/ProfileController.cs
@@ -250,7 +250,7 @@ namespace Classy.DotNet.Mvc.Controllers
             proContactInfo.CompanyContactInfo = proContactInfo.CompanyContactInfo ?? new ExtendedContactInfoView();
             proContactInfo.CompanyContactInfo.Location = proContactInfo.CompanyContactInfo.Location ?? new LocationView();
             proContactInfo.CompanyContactInfo.Location.Address = proContactInfo.CompanyContactInfo.Location.Address ?? new PhysicalAddressView();
-            var proMetadata = profile.IsProfessional ? (profile.Metadata != null ? new TProMetadata().FromDictionary(profile.Metadata, false) : new TProMetadata()) : default(TProMetadata);
+            var proMetadata = profile.IsProfessional ? (profile.Metadata != null ? new TProMetadata().FromDictionary(profile.Metadata, true) : new TProMetadata()) : default(TProMetadata);
             var userMetadata = !profile.IsProfessional ? (profile.Metadata != null ? new TUserMetadata().FromDictionary(profile.Metadata) : new TUserMetadata()) : default(TUserMetadata);
             var model = new EditProfileViewModel<TProMetadata, TUserMetadata>
             {
@@ -962,6 +962,11 @@ namespace Classy.DotNet.Mvc.Controllers
             else
             {
                 translation = profileService.GetTranslation(profileId, cultureCode);
+                if (translation.Metadata != null)
+                {
+                    translation.Metadata["BusinessDescription"] = translation.Metadata.ContainsKey("BusinessDescription") ? (new MarkdownSharp.Markdown()).Transform(translation.Metadata["BusinessDescription"]) : string.Empty;
+                    translation.Metadata["ServicesProvided"] = translation.Metadata.ContainsKey("ServicesProvided") ? (new MarkdownSharp.Markdown()).Transform(translation.Metadata["BusinessDescription"]) : string.Empty;
+                }
             }
 
             if (Request.Headers["Accept"].ToLower().Contains("text/html"))
