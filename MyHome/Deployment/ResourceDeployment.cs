a634a5096be8edd7da4645e284e3da1f2a314818
Juvaly <juvaly@gmail.com>
2014-04-26 15:48:19 +0300

    resource deployment - moved to myhome project

---
+++ MyHome/Deployment/ResourceDeployment.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deployment
+{
+    public class Resource
+    {
+        public string Key { get; set; }
+    }
+    public class ResourceManifest 
+    {
+        public IList<Resource> Resources { get; set;}
+    }
+
+    [TestClass]
+    // THIS IS NOT A TEST:
+    // this class does actual deployment of new resources to the environment where it is running
+    public class ResourceDeployment
+    {
+        [TestMethod]
+        public void DeployNewResources()
+        {
+            Trace.WriteLine("Deploying new reources");
+
+            try 
+            {
+                var resourceManifests = Directory.GetFiles("Resources", "*.resm");
+                foreach(var rm in resourceManifests)
+                {
+                    Trace.WriteLine(string.Format("Found {0}... deploying", rm));
+                
+                    var manifest = GetManifestFromFile(rm); 
+                }
+                Assert.IsTrue(1 == 1);
+            }
+            catch
+            {
+                Assert.IsTrue(1 == 2);
+            }
+        }
+
+        private ResourceManifest GetManifestFromFile(string fileName)
+        {
+            var resmFile = File.OpenText(fileName);
+            var resmContent = resmFile.ReadToEnd();
+            resmFile.Close();
+            resmFile.Dispose();
+            var manifest = Newtonsoft.Json.JsonConvert.DeserializeObject<ResourceManifest>(resmContent);
+            return manifest;
+        }
+    }
+}
