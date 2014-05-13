8b84b8e09c0efc7af36261a7f8464e3863d3e401
gillyb <gillyb@gmail.com>
2014-05-13 14:41:55 +0300

    adding feature switch infrastructure

---
+++ Classy.DotNet.Mvc/ActionFilters/FeatureSwitchFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Classy.DotNet.Mvc.Config;
+
+namespace Classy.DotNet.Mvc.ActionFilters
+{
+    public class FeatureSwitchFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var httpContext = filterContext.HttpContext;
+            if (httpContext == null)
+                return;
+
+            var request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            var featureSwitches = request.QueryString.Get("feature-switch");
+            if (featureSwitches == null || !featureSwitches.Any())
+                return;
+
+            var featuresOn = featureSwitches.ToLower().Split(';');
+            FeatureSwitchProvider.TurnOnFeatures(featuresOn);
+        }
+    }
+}
