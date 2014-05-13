9b2fab6e21f05bbe69daadd2910a29be6bf40058
gillyb <gillyb@gmail.com>
2014-05-13 14:41:55 +0300

    adding feature switch infrastructure

---
+++ Classy.DotNet.Mvc/Config/FeatureSwitchProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Classy.DotNet.Mvc.Config
+{
+    public class FeatureSwitchProvider
+    {
+        public const string FeatureSwitchProviderKey = "__FeatureSwitchProviderKey__";
+
+        public static bool IsFeatureOn(string featureName)
+        {
+            var httpContext = HttpContext.Current;
+            if (httpContext == null)
+                return false;
+
+            var featuresOn = httpContext.Items[FeatureSwitchProviderKey];
+            if (featuresOn == null)
+                return false;
+
+            var featuresList = featuresOn as HashSet<string>;
+            if (featuresList == null)
+                return false;
+
+            return featuresList.Contains(featureName);
+        }
+
+        public static void TurnOnFeatures(string[] featureNames)
+        {
+            var httpContext = HttpContext.Current;
+            if (httpContext == null)
+                return;
+
+            if (httpContext.Items[FeatureSwitchProviderKey] == null)
+                httpContext.Items[FeatureSwitchProviderKey] = new HashSet<string>();
+
+            foreach (var feature in featureNames)
+                ((HashSet<string>) httpContext.Items[FeatureSwitchProviderKey]).Add(feature);
+        }
+    }
+}
