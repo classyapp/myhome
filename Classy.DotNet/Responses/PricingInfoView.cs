0c40c427bb94f49d4af4b84eb77e7af9f568ed80
Michael Bar <barmic@gmail.com>
2014-05-16 17:38:43 -0300

    Initial commit for reabse

--- Classy.DotNet/Responses/PricingInfoView.cs
+++ Classy.DotNet/Responses/PricingInfoView.cs
@@ -8,20 +8,6 @@ namespace Classy.DotNet.Responses
 {
     public class PricingInfoView
     {
-        // pricing
-        public string SKU { get; set; }
-        public double? Price { get; set; }
-        public double? CompareAtPrice { get; set; }
-
-        // inventory
-        public int Quantity { get; set; }
-
-        // purchase options (size, color, etc)
         public IList<PurchaseOptionView> PurchaseOptions { get; set; }
-
-        // TODO: make this a list of shipping options? 
-        public int? DomesticRadius { get; set; }
-        public decimal? DomesticShippingPrice { get; set; }
-        public decimal? InternationalShippingPrice { get; set; }
     }
 }
