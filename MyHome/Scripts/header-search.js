692b4392637dede011ca609dac8925869b8359e8
Gilly Barr <gillyb@gmail.com>
2014-05-21 10:29:14 +0300

    profile auto suggest links to profile page

---
+++ MyHome/Scripts/header-search.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    var listingsSuggestions = new Bloodhound({
+        name: 'listings-suggestions',
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('Value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: '',
+        remote: 'search/listings/suggest?q=%QUERY'
+    });
+    var profilesSuggestions = new Bloodhound({
+        name: 'profiles-suggestions',
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('Value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: '',
+        remote: 'search/profiles/suggest?q=%QUERY'
+    });
+
+    listingsSuggestions.initialize();
+    profilesSuggestions.initialize();
+
+    $('#q.typeahead').typeahead({
+        minLength: 2,
+        hightlight: true
+    }, {
+        name: 'listing-suggestions',
+        displayKey: 'Value',
+        source: listingsSuggestions.ttAdapter(),
+        templates: {
+            header: '<span class=\"tt-suggestion-header\">@Localizer.Get("HeaderSearch_ListingsSuggestionsHeader")</span>'
+        }
+    }, {
+        name: 'profile-suggestions',
+        displayKey: 'Value',
+        source: profilesSuggestions.ttAdapter(),
+        templates: {
+            header: '<span class=\"tt-suggestion-header\">@Localizer.Get("HeaderSearch_ProfilesSuggestionsHeader")</span>'
+        }
+    });
+
+    $(document).bind('typeahead:selected', function (event, suggestion, dataset) {
+        if (dataset == 'profile-suggestions')
+            window.location.href = Classy.UrlBuilder.ProfilePage(suggestion.Key, suggestion.Value);
+        else
+            $('#navbar-search').submit();
+    });
+});
