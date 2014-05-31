cf83b89f21741fe9e6c920c49af2489ab66a231b
juvaly <juvaly@gmail.com>
2014-05-31 09:20:38 +0300

    added related pros and listings

---
+++ Classy.DotNet.Mvc/Controllers/LoadPublicProfileEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Classy.DotNet.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classy.DotNet.Mvc.Controllers
+{
+    public class LoadPublicProfileEventArgs<TProMetadata>
+    {
+        public ProfileView Profile;
+        public TProMetadata TypedMetadata;
+        public IList<ProfileView> RelatedProfiles { get; set; }
+        public IList<ListingView> RelatedListings { get; set; }
+    }
+}
