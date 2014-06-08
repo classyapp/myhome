6167aa07a22a2c86220b196afedfb1dcb3e0b3f2
gillyb <gillyb@gmail.com>
2014-06-08 09:16:31 +0300

    adding perf metrics filter

---
+++ Classy.DotNet.Mvc/ActionFilters/PerfMetricsFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Classy.DotNet.Mvc.Config;
+using Classy.DotNet.Mvc.Config.PerfMetrics;
+using Classy.DotNet.Mvc.Extensions;
+
+namespace Classy.DotNet.Mvc.ActionFilters
+{
+    public class PerfMetricsFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var httpContext = filterContext.HttpContext;
+            if (httpContext == null)
+                return;
+
+            var request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            var perfFlag = request.QueryString.Get("perf");
+            if (perfFlag == null || perfFlag != true.ToString().ToLower())
+                return;
+
+            httpContext.Items[PerfMetricsProvider.PerfMetricsEnabledKey] = true.ToString();
+        }
+    }
+}
