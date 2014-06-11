513564f39e07913dee862651c00bef1fe35c3fe2
Gilly Barr <gillyb@gmail.com>
2014-06-11 13:14:12 +0300

    starting to work...

--- MyHome/Scripts/mobile/profile-module.js
+++ MyHome/Scripts/mobile/profile-module.js
@@ -1,14 +1,19 @@
 
-angular.module('profile-module', ['ngRoute'])
+var profileApp = angular.module('profileApp', []);
 
-//.value('varName', { someVar: 'someValue' }) // way to inject objects into module controllers
+profileApp.value('apiUrl', 'http://www.thisisclassy.com:8008'); // way to inject objects into module controllers
 
-.config(function($routeProvider) {
+var config = {
+    headers: {
+        'X-Classy-Env': '{ "AppId": "v1.0" }',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+};
 
-})
-.controller('ProfileController', function ($scope, $http) {
-    debugger;
-    $http.get('someUrl/profile/data').success(function(data) {
+profileApp.controller('ProfileController', function ($scope, $http, apiUrl) {
+    $scope.profileDetails = { 'UserName': 'Gilly' };
+    $http.get(apiUrl + '/profile/246', config).success(function(data) {
         $scope.profileDetails = data;
     }).error(function() {
         // TODO: display some error message
