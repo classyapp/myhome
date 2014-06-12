1df257de585d401ebe5122bd41349b1615453522
Gilly Barr <gillyb@gmail.com>
2014-06-12 16:09:16 +0300

    added support for collage thumbnails

--- MyHome/Scripts/mobile/utilities.js
+++ MyHome/Scripts/mobile/utilities.js
@@ -7,6 +7,23 @@ classyUtilitiesService.factory('ClassyUtilities', [function() {
             Thumbnail: function(appSettings, imageKey, width, height) {
                 var cdnUrl = appSettings.CdnUrl;
                 return cdnUrl + '/thumbnail/' + imageKey + '?Width=' + width + '&Height=' + height + '&format=json';
+            },
+            Thumbnails: function (appSettings, imageKeys, width, height) {
+                // TODO: take width/height into consideration!
+                var sizes = [
+                    [{ x: 270, y: 220 }],
+                    [{ x: 135, y: 220 }, { x: 135, y: 220 }],
+                    [{ x: 200, y: 220 }, { x: 70, y: 110 }, { x: 70, y: 110 }],
+                    [{ x: 200, y: 220 }, { x: 70, y: 70 }, { x: 70, y: 70 }, { x: 70, y: 70 }]
+                ];
+                var count = imageKeys.length;
+                var container = "<div class=\"thumb" + count + "\">";
+                for (var i = 0; i < count; i++) {
+                    var imageUrl = appSettings.CdnUrl + "/thumbnail/" + imageKeys[i] + "?Width=" + sizes[i].X + "&Height=" + sizes[i].Y + "&format=json";
+                    container += "<div class=\"thumb\" style=\"background-image:url(" + imageUrl + ")></div>";
+                }
+                container += "</div>";
+                return container;
             }
         }
     };
