a8a3a368c8dc50a1aca0ebaa03f7a6d314eb80db
Gilly Barr <gillyb@gmail.com>
2014-06-12 15:28:02 +0300

    added reviews to profile page

--- MyHome/Scripts/mobile/profile-page.js
+++ MyHome/Scripts/mobile/profile-page.js
@@ -17,18 +17,32 @@ profilePage.controller('ProfileController', function ($scope, $http, AppSettings
         var appSettings = Classy.CacheProvider.Get("__AppSettings__");
         var utilities = ClassyUtilities;
 
-        $http.get(appSettings.ApiUrl + '/profile/246?includeCollections=true', config).success(function (data) {
+        $http.get(appSettings.ApiUrl + '/profile/246?includeCollections=true', config).success(function(data) {
             $scope.profileDetails = data;
-            
+
             // organize collections
             var collectionImages = [];
-            $scope.profileDetails.Collections.forEach(function (collection) {
+            $scope.profileDetails.Collections.forEach(function(collection) {
                 if (collection.CoverPhotos && collection.CoverPhotos.length > 0 && collection.CoverPhotos[0].trim() != '')
                     collectionImages.push({ Image: utilities.Images.Thumbnail(appSettings, collection.CoverPhotos[0], 200, 200) });
             });
             $scope.Collections = collectionImages;
 
             $scope.Location = getProfileLocation(data);
+            $scope.Rating = getRatingAsArray(data.ReviewAverageScore);
+
+            // reviews
+            var reviews = [];
+            data.Reviews.forEach(function(review) {
+                reviews.push({
+                    ReviewerId: review.ProfileId,
+                    Name: review.ReviewerUsername,
+                    Avatar: review.ReviewerThumbnailUrl,
+                    Content: review.Content,
+                    Rating: getRatingAsArray(review.Score)
+                });
+            });
+            $scope.Reviews = reviews;
 
         }).error(function () {
             // TODO: display some error message
@@ -44,6 +58,13 @@ profilePage.controller('ProfileController', function ($scope, $http, AppSettings
             var address = location.Address;
             if (!address) return '';
             return address.City + ', ' + address.Country;
+        }
+
+        function getRatingAsArray(rating) {
+            var ratings = [];
+            for (var i = 0; i < 5; i++)
+                ratings.push({ id: i, star: (rating > i ? true : false) });
+            return rating;
         }
 
     });
