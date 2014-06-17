5f52146423c2ae76bc8aece62c225eadd2379c15
Gilly Barr <gillyb@gmail.com>
2014-06-17 18:08:33 +0300

    started styling collection page

--- MyHome/Scripts/mobile/profile-page.js
+++ MyHome/Scripts/mobile/profile-page.js
@@ -170,79 +170,48 @@ profilePage.controller('ProfileController', function ($scope, $http, AppSettings
     });
 });
 
-profilePage.controller('CollectionController', function ($scope, $http, AppSettings, ClassyUtilities, Localizer, $routeParams) {
-    $scope.direction = 'left';
-    $scope.currentIndex = 0;
+profilePage.controller('CollectionController', function($scope, $http, AppSettings, ClassyUtilities, Localizer, $routeParams) {
+    AppSettings.then(function(appSettings) {
 
-    $scope.setCurrentListingIndex = function (index) {
-        $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-        $scope.currentIndex = index;
-    };
+        $http.get(appSettings.ApiUrl + '/collection/' + $routeParams.collectionId + '?includeComments=true&includeCommenterProfiles=true&includeListings=true&increaseViewCounter=true&includeProfile=true', config).success(function (data) {
 
-    $scope.isCurrentListingIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
+            $scope.Avatar = data.Profile.Avatar.Url;
+            $scope.CollectionName = data.Title;
 
-    $scope.prevListing = function () {
-        $scope.direction = 'left';
-        $scope.currentIndex = ($scope.currentIndex < $scope.Listings.length - 1) ? ++$scope.currentIndex : 0;
-    };
+            $scope.ViewCount = data.ViewCount;
+            $scope.FavoriteCount = data.FavoriteCount;
+            $scope.CommentCount = data.CommentCount;
 
-    $scope.nextListing = function () {
-        $scope.direction = 'right';
-        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.Listings.length - 1;
-    };
+            var imageWidth = ClassyUtilities.Screen.GetWidth() - 80;
+            var listings = [];
+            data.Listings.forEach(function(listing) {
+                listings.push({
+                    Title: listing.Title,
+                    ImageUrl: ClassyUtilities.Images.Thumbnail(appSettings, listing.ExternalMedia[0].Key, imageWidth, 500)
+                });
+            });
+            $scope.Listings = listings;
 
-    AppSettings.then(function (appSettings) {
+            var comments = [];
+            data.Comments.forEach(function(comment) {
+                comments.push({
+                    Commenter: comment.Profile.UserName,
+                    CommenterId: comment.ProfileId,
+                    CommenterAvatarUrl: comment.Profile.Avatar.Url,
+                    Content: comment.Content
+                });
+            });
+            $scope.Comments = comments;
 
-        $http.get(appSettings.ApiUrl + '/collection/' + $routeParams.collectionId + '?includeListings=true&increaseViewCounter=true', config).success(function (data) {
-
-            $scope.Listings = data.Listings;
-
-        }).error(function () {
+        }).error(function() {
             // TODO: display some error message
         });
 
         // get localized resources
         $scope.Resources = {};
-        //Localizer.Get('Mobile_ProfilePage_ViewAllProjects').then(function (resource) {
-        //    $scope.Resources.ViewAllProjects = resource;
-        //});
+        Localizer.Get('Mobile_CollectionPage_ViewAllComments').then(function (resource) {
+            $scope.Resources.ViewAllComments = resource;
+        });
 
     });
-}).animation('.slide-animation', function () {
-    return {
-        addClass: function (element, className, done) {
-            var scope = element.scope();
-
-            if (className == 'ng-hide') {
-                var finishPoint = element.parent().width();
-                if (scope.direction !== 'right') {
-                    finishPoint = -finishPoint;
-                }
-                TweenMax.to(element, 0.5, { left: finishPoint, onComplete: done });
-            }
-            else {
-                done();
-            }
-        },
-        removeClass: function (element, className, done) {
-            var scope = element.scope();
-
-            if (className == 'ng-hide') {
-                element.removeClass('ng-hide');
-
-                var startPoint = element.parent().width();
-                if (scope.direction === 'right') {
-                    startPoint = -startPoint;
-                }
-
-                TweenMax.set(element, { left: startPoint });
-                TweenMax.to(element, 0.5, { left: 0, onComplete: done });
-            }
-            else {
-                done();
-            }
-        }
-    };
 });
