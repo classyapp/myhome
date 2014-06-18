38c1be0302390f496ecbdd1e5b5c8beef0834858
Gilly Barr <gillyb@gmail.com>
2014-06-18 15:01:29 +0300

    changing the poll details page layout around
    and adding interesting stuff so it wont be a dead end

--- MyHome/Scripts/classy.js
+++ MyHome/Scripts/classy.js
@@ -103,9 +103,9 @@ String.prototype.decodeHTML = function () {
 
 Classy.UnveilImages = function () {
     $('img[data-rel="thumbnail"]').unveil(200, function () {
-        $(this).load(function () {
+        $(this).load(function() {
             this.style.opacity = 1;
-        })
+        });
     }).error(function () { $(this).attr("src", "/img/missing-thumb.png") });
 };
 
