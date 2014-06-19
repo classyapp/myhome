e78e5a52315aee29040ceaa4ad83df92fd3f7ca6
gillyb <gillyb@gmail.com>
2014-06-19 12:17:11 +0300

    added urls for mobile sharing

--- MyHome/Scripts/mobile/common.js
+++ MyHome/Scripts/mobile/common.js
@@ -9,3 +9,19 @@ Classy.Utilities = {
         return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 };
+
+Classy.Share = function (network, url) {
+    if (!url) url = window.location.href;
+    var protocol = window.location.protocol;
+    switch (network) {
+        case 'facebook':
+            window.location.href = protocol + '//www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url) + '&utm_source=share_image&utm_medium=facebook';
+            break;
+        case 'twitter':
+            window.location.href = protocol + '//twitter.com/home?status=' + encodeURIComponent(url) + '&utm_source=share_image&utm_medium=twitter';
+            break;
+        case 'google':
+            window.location.href = protocol + '//plus.google.com/share?url=' + encodeURIComponent(url) + '?utm_source=share_image&utm_medium=gplus';
+            break;
+    }
+};
