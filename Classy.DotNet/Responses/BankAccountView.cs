e134936e46a3a11c4905ca5e6894b50c3e46c93a
gillyb <gillyb@gmail.com>
2014-07-02 09:48:03 +0300

    converting prices to decimal

--- Classy.DotNet/Responses/BankAccountView.cs
+++ Classy.DotNet/Responses/BankAccountView.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Classy.DotNet.Responses
+﻿namespace Classy.DotNet.Responses
 {
     public class BankAccountView
     {
