f7ca81c76a30efd978a2ce4a8abca4a92a1e331e
gillyb <gillyb@gmail.com>
2014-07-16 09:02:40 +0300

    minor cleanup

--- Classy.DotNet/Services/SearchService.cs
+++ Classy.DotNet/Services/SearchService.cs
@@ -13,7 +13,6 @@ namespace Classy.DotNet.Services
         private static readonly string GET_SEARCH_PROFILES_SUGGESTIONS = ENDPOINT_BASE_URL + "/search/profile/suggest?q={0}";
         private static readonly string GET_SEARCH_PRODUCTS_SUGGESTIONS = ENDPOINT_BASE_URL + "/search/product/suggest?q={0}";
         private static readonly string GET_SEARCH_KEYWORDS_SUGGESTIONS = ENDPOINT_BASE_URL + "/search/keywords/suggest?q={0}&lang={1}";
-        private static readonly string GET_SEARCH_PRODUCTS = ENDPOINT_BASE_URL + "/products/search";
         
         public List<SearchSuggestion> SearchListingsSuggestions(string q)
         {
