d4caf3bbabd18f54895729f7f9e9341f3d814886
gillyb <gillyb@gmail.com>
2014-07-20 22:05:59 +0300

    fixing config.js for test deployment

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -1,7 +1,7 @@
 ﻿{
     "AppId": "v1.0",
     "Host": "//local.homelab:8080",
-    "ApiUrl": "//myhome-3.apphb.com",
+    "ApiUrl": "//marketplaceapi.apphb.com",
     "CdnUrl": "//d107oye3n9eb07.cloudfront.net",
     "DefaultCulture": "en"
 }
