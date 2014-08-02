b021be2485f14e256316b0fa18dd398389fb82bf
gillyb <gillyb@gmail.com>
2014-08-02 13:50:48 +0300

    home header

--- MyHome/Scripts/mobile/directives/classy-home-header.js
+++ MyHome/Scripts/mobile/directives/classy-home-header.js
@@ -1,17 +1,23 @@
-classy.directive('classyHomeHeader', function ($location, AuthProvider) {
+classy.directive('classyHomeHeader', function($location, AppSettings, AuthProvider, ClassyUtilities) {
     return {
         restrict: 'E',
         templateUrl: 'Home/classy-header.html',
         transclude: true,
-        link: function (scope) {
-            AuthProvider.getUser().then(function (data) {
-                scope.IsAuthenticated = data.IsAuthenticated;
-                scope.User = data.Profile;
-            });
+        link: function(scope) {
+            AppSettings.then(function(appSettings) {
+                AuthProvider.getUser().then(function(data) {
+                    scope.IsAuthenticated = data.IsAuthenticated;
+                    scope.User = data.Profile;
+                    scope.User.Avatar.ImageUrl = ClassyUtilities.Images.Thumbnail(appSettings, data.Profile.Avatar.Key, 45, 45);
+                });
 
-            scope.loginPage = function() {
-                $location.url('/Login');
-            };
+                scope.loginPage = function() {
+                    $location.url('/Login');
+                };
+                scope.profilePage = function(userId) {
+                    $location.url('/Profile/' + userId);
+                };
+            });
         }
     };
 });
