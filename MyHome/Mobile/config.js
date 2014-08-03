4edf492d83ef93e8654fb9928623402da51048bd
gillyb <gillyb@gmail.com>
2014-08-03 15:33:38 +0300

    adding homepage configuration

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -3,5 +3,12 @@
     "Host": "//myhome-3.apphb.com",
     "ApiUrl": "//marketplaceapi.apphb.com",
     "CdnUrl": "//d107oye3n9eb07.cloudfront.net",
-    "DefaultCulture": "en"
+    "DefaultCulture": "en",
+    "HomePage": {
+        "HomeHeader" : true,
+        "Listing" : "5314ecfca3a75d1aec7b0de4",
+        "Collection" : "5314ecfca3a75d1aec7b0de3",
+        "Article" : "5352d6dbaa05a623b456d2b8",
+        "Html" : "<div class=\"testing html block\">This is just a cool test</div>"
+    }
 }
