44a5a0b9cc6ef42368fdd4716e79a005994d5bc9
gillyb <gillyb@gmail.com>
2014-08-03 21:17:10 +0300

    added search functionality to header

--- MyHome/Scripts/mobile/directives/classy-home-header.js
+++ MyHome/Scripts/mobile/directives/classy-home-header.js
@@ -1,4 +1,4 @@
-classy.directive('classyHomeHeader', function($location, AppSettings, AuthProvider, ClassyUtilities) {
+classy.directive('classyHomeHeader', function($location, $http, AppSettings, AuthProvider, ClassyUtilities) {
     return {
         restrict: 'E',
         templateUrl: 'Home/classy-header.html',
@@ -21,12 +21,47 @@ classy.directive('classyHomeHeader', function($location, AppSettings, AuthProvid
                     $location.url('/Profile/' + userId);
                 };
 
+                var searchInput = $('#header-search');
                 scope.displaySearch = function() {
-                    $('#header-search').removeClass('hidden').focus().blur(scope.hideSearch);
+                    searchInput.removeClass('hidden').focus().blur(scope.hideSearch);
                 };
                 scope.hideSearch = function() {
-                    $('#header-search').addClass('hidden');
+                    searchInput.addClass('hidden');
                 };
+
+                searchInput.keyup(function (e) {
+                    var query = $(this).val().trim();
+                    if (e.keyCode == 13 && query != '') {
+                        window.location.href = '//' + $location.host() + ':' + $location.port() + '/' + $location.path() + '#/Search?q=' + $(this).val().trim();
+                    }
+                    if (query.length >= 2) {
+                        $http.get(window.location.protocol + appSettings.Host + '/mobile/search/suggest?q=' + query, config).success(function (data) {
+                            scope.AutoSuggestions = data;
+                            scope.$apply();
+                        });
+                    } else {
+                        scope.AutoSuggestions = [];
+                        scope.$apply();
+                    }
+                });
+
+                $('body').delegate('.auto-suggestions .suggestion', 'click', function () {
+                    var query = $(this).find('.content').data('suggestion-value');
+                    var section = $(this).closest('.suggestion-section').find('.title').html();
+                    switch (section) {
+                        case 'Rooms':
+                            window.location.href = '//' + $location.host() + ':' + $location.port() + window.location.pathname + '#/Search?room=' + query;
+                            break;
+                        case 'Styles':
+                            window.location.href = '//' + $location.host() + ':' + $location.port() + window.location.pathname + '#/Search?style=' + query;
+                            break;
+                        case 'Professionals':
+                            window.location.href = '//' + $location.host() + ':' + $location.port() + window.location.pathname + '#/Profile/' + query;
+                            break;
+                        case 'Product Categories':
+                            window.location.href = '//' + $location.host() + ':' + $location.port() + window.location.pathname + '#/Search?category=' + query;
+                    }
+                });
             });
         }
     };
