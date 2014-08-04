7be22246fc218ec4aa30071c7ee093bb60099b63
Gilly Barr <gillyb@gmail.com>
2014-08-04 12:19:25 +0300

    initial swiper

--- MyHome/Scripts/mobile/controllers/home-controller.js
+++ MyHome/Scripts/mobile/controllers/home-controller.js
@@ -14,7 +14,7 @@ classy.controller('HomeController', function ($scope, $http, $compile, AppSettin
                         newElement = $compile("<classy-home-header></classy-home-header>")($scope);
                         break;
                     case "Listing":
-                        newElement = $compile("<classy-listing listing-id=\"" + homePageSettings[elem] + "\"></classy-listing>")($scope);
+                        newElement = $compile("<classy-listing listing-ids=\"" + homePageSettings[elem] + "\"></classy-listing>")($scope);
                         break;
                     case "Collection":
                         newElement = $compile("<classy-collection collection-id=\"" + homePageSettings[elem] + "\"></classy-collection>")($scope);
@@ -30,14 +30,7 @@ classy.controller('HomeController', function ($scope, $http, $compile, AppSettin
                     mainContainer.append(newElement);
             }
         }
-
-        //$scope.listingId = '5314ecfca3a75d1aec7b0de4';
-        //$scope.collectionId = '5314ecfca3a75d1aec7b0de3';
-        //$scope.articleId = '5352d6dbaa05a623b456d2b8';
-
-        //var newElement = $compile("<classy-listing listing-id=\"listingId\"></classy-listing>")($scope);
-        //$('.main-container').append(newElement);
-
+        
     });
 
 });
--- MyHome/Scripts/mobile/directives/classy-listing.js
+++ MyHome/Scripts/mobile/directives/classy-listing.js
@@ -1,17 +1,37 @@
-classy.directive('classyListing', function ($http, AppSettings, ClassyUtilities) {
+classy.directive('classyListing', function ($http, $q, AppSettings, ClassyUtilities) {
     return {
         restrict: 'E',
         templateUrl: 'Home/classy-listing.html',
         scope: {
-            listingId: '@listingId'
+            listingIds: '@listingIds'
         },
         link: function (scope) {
             var w = ClassyUtilities.Screen.GetWidth();
-            AppSettings.then(function(appSettings) {
-                $http.get(appSettings.ApiUrl + '/listing/' + scope.listingId + '?includeProfile=true', config).success(function (data) {
-                    scope.ImageUrl = ClassyUtilities.Images.Thumbnail(appSettings, data.ExternalMedia[0].Key, w, 300);
-                    scope.CopyrightMessage = ClassyUtilities.Listing.GetCopyrightMessage(data);
+            AppSettings.then(function (appSettings) {
+
+                var listings = scope.listingIds.split(',');
+                var featuredListings = [];
+
+                var promises = [];
+
+                listings.forEach(function(listingId) {
+                    var q = $http.get(appSettings.ApiUrl + '/listing/' + listingId + '?includeProfile=true', config).success(function (data) {
+                        featuredListings.push({
+                            ImageUrl: ClassyUtilities.Images.Thumbnail(appSettings, data.ExternalMedia[0].Key, w, 300),
+                            CopyrightMessage: ClassyUtilities.Listing.GetCopyrightMessage(data)
+                        });
+                    });
+                    promises.push(q);
                 });
+                scope.FeaturedListings = featuredListings;
+
+                $q.all(promises).then(function() {
+                    $('.swiper-container').swiper({
+                        mode: 'horizontal',
+                        loop: true
+                    });
+                });
+
             });
         }
     };
