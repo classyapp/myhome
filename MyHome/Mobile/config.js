7445d896988357aa157591cf9737a4401aaecd6f
Gilly Barr <gillyb@gmail.com>
2014-08-04 14:29:02 +0300

    updating config for master deployment

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -1,12 +1,12 @@
 ﻿{
     "AppId": "v1.0",
-    "Host": "//myhome-3.apphb.com",
+    "Host": "//www.homelab.com",
     "ApiUrl": "//liveapi.apphb.com",
     "CdnUrl": "//d107oye3n9eb07.cloudfront.net",
     "DefaultCulture": "en",
     "HomePage": {
         "HomeHeader" : true,
-        "Listing" : "5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4,5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4",
+        "Listing" : "533ebff2c1d27f9144f5f5ab:533e7c1dc1d27f6f5c3879fd,53949dfbaa05a610404fc0ca:53104078a3a75d3a1c9f6b65",
         "Collection" : "5314ecfca3a75d1aec7b0de3",
         "Article" : "5352d6dbaa05a623b456d2b8",
         "Html" : "<div class='testing html block'>This is just a cool test</div>"
