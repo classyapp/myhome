025c4d32426d6364147a3a2b1772cccf184c9800
gillyb <gillyb@gmail.com>
2014-08-19 22:51:14 +0300

    removed html test text from homepage

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -8,7 +8,6 @@
         "HomeHeader" : true,
         "Listing" : "5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4,5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4",
         "Collection" : "5314ecfca3a75d1aec7b0de3",
-        "Article" : "5352d6dbaa05a623b456d2b8",
-        "Html" : "<div class='testing html block'>This is just a cool test</div>"
+        "Article" : "5352d6dbaa05a623b456d2b8"
     }
 }
