34ccf859425b44cf62d38dcaad66c5e406a00604
juvaly <juvaly@gmail.com>
2014-08-27 17:30:28 +0300

    Update config.js

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -9,6 +9,6 @@
         "Listing" : "534e7778aa05a6961c65cdca:534e777daa05a6961c65cdd1,5339275dc1d27f2ba4a0ebf1:5339276fc1d27f2ba4a0ebfe,5314ad98a3a75d914c335f01:5314ae4ba3a75d914c335f61",
         "Collection" : "5368ea02aa05a61b440dd590",
         "Article" : "5352d6dbaa05a623b456d2b8",
-        "Collection" : "534d5d64aa05a69e34f1dd9d"
+        "Collection" : "5368ea02aa05a61b440dd590"
     }
 }
