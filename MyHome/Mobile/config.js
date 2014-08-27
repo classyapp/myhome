c6a0ea02f124ecfa43fa7889a24a424ea7b4ed77
juvaly <juvaly@gmail.com>
2014-08-27 09:59:12 +0300

    change mobile home page content

--- MyHome/Mobile/config.js
+++ MyHome/Mobile/config.js
@@ -6,8 +6,9 @@
     "DefaultCulture": "en",
     "HomePage": {
         "HomeHeader" : true,
-        "Listing" : "5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4,5314ecfca3a75d1aec7b0de3:5314ecfca3a75d1aec7b0de4",
-        "Collection" : "5314ecfca3a75d1aec7b0de3",
-        "Article" : "5352d6dbaa05a623b456d2b8"
+        "Listing" : "534e7778aa05a6961c65cdca:534e777daa05a6961c65cdd1,5339275dc1d27f2ba4a0ebf1:5339276fc1d27f2ba4a0ebfe,5314ad98a3a75d914c335f01:5314ae4ba3a75d914c335f61",
+        "Collection" : "5368ea02aa05a61b440dd590",
+        "Article" : "5352d6dbaa05a623b456d2b8",
+        "Collection" : "534d5d64aa05a69e34f1dd9d"
     }
 }
